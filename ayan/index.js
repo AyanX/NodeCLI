a25f5dbcfc56cf5568b5c4909e72d88d1ae9c8a4
AyanX <fearthesorcerer@gmail.com>
2025-06-05 23:49:36 +0300

    added clear console

--- ayan/index.js
+++ ayan/index.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 /*add a shebang to let the code know it runs with node */
 
+const clearConsole = require('clear-any-console');
+
+// Clears the console.
+clearConsole();
 
 console.log(`hello, am ayan
     
